eb96c97a4c319c424950333267fb482ba761e46e
zoujoe <120407910+zoujoe@users.noreply.github.com>
2020-08-14 07:47:57 +0000

    Atualização do README.md

--- components/ArticleContent.vue
+++ components/ArticleContent.vue
@@ -8,4 +8,15 @@
 export default {
   name: 'ArticleContent',
   props: {
- 
+    wider: Boolean
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.article-content {
+  width: 30vw;
+  &.wider {
+    width: auto;
+    m
