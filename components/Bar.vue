32bb6461dcb04e9a37aef4f9d3bb96912ba2276c
zoujoe <120407910+zoujoe@users.noreply.github.com>
2020-08-17 06:03:07 +0000

    further refactoring

--- components/Bar.vue
+++ components/Bar.vue
@@ -1,2 +1,10 @@
 <template>
-  <div class
+  <div class="bar" :class="[{visible}]">
+    <span>
+      <slot>Bar content</slot>
+    </span>
+    <div class="prompt-container">
+      <div class="prompt">
+        <regular-button type="primary">
+          Yes
+        </
