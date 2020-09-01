9f2c9c44240dbfe13e6da2c50726a2071b57984a
zoujoe <120407910+zoujoe@users.noreply.github.com>
2020-09-01 20:00:55 +0000

    Adding some docs

--- components/BoxLabel.vue
+++ components/BoxLabel.vue
@@ -1,2 +1,11 @@
 <template>
-  
+  <label :class="[{includeRightMargin}, colourMode]" class="box-label">
+    <slot>box-label content</slot>
+  </label>
+</template>
+
+<script>
+export default {
+  name: 'BoxLabel',
+  props: {
+    i
