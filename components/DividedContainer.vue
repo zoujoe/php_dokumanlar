9e4de799bd62aef0c251cfe8bb3e791e710edab4
zoujoe <120407910+zoujoe@users.noreply.github.com>
2020-09-04 16:34:21 +0000

    Post release cleanup

--- components/DividedContainer.vue
+++ components/DividedContainer.vue
@@ -1,2 +1,7 @@
 <template>
-  <div :class="[{redBorder, shadow, conservativeMobileView}, colourMode]" class="divided-conta
+  <div :class="[{redBorder, shadow, conservativeMobileView}, colourMode]" class="divided-container">
+    <div class="container-content-left" :class="[{hideLeftColumn}]">
+      <slot name="left" />
+    </div>
+    <div class="divider" :class="[{hideLeftColumn}, colourMode]" />
+    <div cl
