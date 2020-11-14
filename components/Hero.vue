aa31e82fbac34ea1e60558111c23862cd67c329f
zoujoe <120407910+zoujoe@users.noreply.github.com>
2020-11-14 16:30:32 +0000

    Fix missing newline

--- components/Hero.vue
+++ components/Hero.vue
@@ -1,3 +1,14 @@
 <template>
   <div class="hero-container" :class="{fullscreen}">
-    <div class="hero-background" :class="{fullscreen}" :style=
+    <div class="hero-background" :class="{fullscreen}" :style="{ backgroundImage: `url(${background})` }" />
+    <div class="hero" :class="{narrow, fullscreen}">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Hero',
+  props: {
+    b
