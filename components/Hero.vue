1c63b91b9bdaa1968b5a19b4c5050efd539b8ba6
zoujoe <120407910+zoujoe@users.noreply.github.com>
2020-11-26 21:25:43 +0000

    Bump to version 3.0.0

--- components/Hero.vue
+++ components/Hero.vue
@@ -20,4 +20,17 @@ export default {
   },
   computed: {
     colourMode () {
-      return this.$store.state.theme.colourMo
+      return this.$store.state.theme.colourMode
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.hero-container {
+  width: 100%;
+}
+.hero {
+  width: 50vw;
+  transition: all
