6111f2035bb3fa00e1d2287b6c42c5fd416f9e3f
zoujoe <120407910+zoujoe@users.noreply.github.com>
2020-12-30 05:54:22 +0000

    Testing easier local image use

--- components/InputLabel.vue
+++ components/InputLabel.vue
@@ -1,3 +1,16 @@
 <template>
   <p :class="[{error, noTopMargin}, colourMode]" class="input-label">
-    <slot>input-label co
+    <slot>input-label content</slot>
+  </p>
+</template>
+
+<script>
+export default {
+  name: 'InputLabel',
+  props: {
+    error: Boolean,
+    noTopMargin: Boolean
+  },
+  computed: {
+    colourMode () {
+      return this.$store.
