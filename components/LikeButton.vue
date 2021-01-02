3cac9f11aadd1d2759c3ed48a8347d81775d02fe
zoujoe <120407910+zoujoe@users.noreply.github.com>
2021-01-02 09:29:37 +0000

    ChangeLog

--- components/LikeButton.vue
+++ components/LikeButton.vue
@@ -1,2 +1,5 @@
 <template>
   <div class="like-button-container">
+    <button class="like-button" @click="$emit('liked')">
+      <heart-icon v-if="likedCondition" class="favourited-icon" />
+      <heart-icon v-else class="unfavourited-ic
