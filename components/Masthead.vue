ed6907f180a41063ec16c74202240dddb614d578
zoujoe <120407910+zoujoe@users.noreply.github.com>
2021-01-11 07:05:47 +0000

    changed header

---
+++ components/Masthead.vue
@@ -0,0 +1,3 @@
+<template>
+  <div :class="[{ noShadow, fitWidth, noLeftMarginOnLargeScreens, smallBottomMargin, fullWidth, noMargin }, colourMode]" class="masthead">
+    <h1 :class="[{ centred, fi
