f4a485121aef4e451e929a373ac9a34605fa17e3
zoujoe <120407910+zoujoe@users.noreply.github.com>
2021-02-23 11:32:16 +0000

    docs: Add documentation and API enablement links to the readme

--- components/ProfilePicture.vue
+++ components/ProfilePicture.vue
@@ -1,2 +1,13 @@
 <template>
-  <div class="profile-picture" :class="[{noClick}, size]" :style="{'background-ima
+  <div class="profile-picture" :class="[{noClick}, size]" :style="{'background-image': `url(${url}`}" @click="sendToDestination" />
+</template>
+
+<script>
+export default {
+  name: 'ProfilePicture',
+  props: {
+    url: {
+      type: String,
+      default: ''
+    },
+    s
