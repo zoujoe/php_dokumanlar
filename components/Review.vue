5ccd8b28431e97071677aa26714bc89e37fd5c9a
zoujoe <120407910+zoujoe@users.noreply.github.com>
2021-04-08 08:08:17 +0000

    Linux 3.4.75

--- components/Review.vue
+++ components/Review.vue
@@ -2,3 +2,138 @@
 <template>
   <div class="review" :class="{ 'alternate': index % 2 === 1 }">
     <div class="flex-container">
+      <div>
+        <profile-picture :username="review.username" class="review-avatar" size="small" :url="review.avatar" />
+      </div>
+      <div>
+        <subheading smallest>
+          <nuxt-link class="review-author" :to="'/user/' + review.username">
+            {{ review.username }}
+          </nuxt-link>
+        </subheading>
+        <subheading smaller>
+          {{ review.header }}
+        </subheading>
+        <div v-for="(block, i) in review.body" :key="block.id">
+          <paragraph v-if="block.type === 'paragraph'" :always-show="i === 0" :truncate="truncationLength" @toTruncate="truncationLength = Number($event); truncated = true" />
+        </div>
+        <a v-if="truncated" :href="`#${review.author}`" @click="$emit('show')">
+          Read more
+        </a>
+        <like-button :liked-condition="user === null || review.likers.some(liker => liker.id === user.id)" :likes="review.likes" :likers="review.likers" @liked="$emit('liked')" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import profilePicture from '~/components/ProfilePicture.vue'
+import subheading from '~/components/Subheading.vue'
+import paragraph from '~/components/Paragraph.vue'
+import likeButton from '~/components/LikeButton.vue'
+
+export default {
+  name: 'Review',
+  components: {
+    profilePicture,
+    subheading,
+    paragraph,
+    likeButton
+  },
+  props: {
+    reviewProp: {
+      type: Object,
+      default: () => {
+        return {
+          author: 'loading',
+          avatar: 'loading',
+          body: 'loading',
+          header: 'loading',
+          likers: [],
+          likes: 0
+        }
+      }
+    },
+    release: {
+      type: String,
+      default: ''
+    },
+    index: {
+      type: Number,
+      default: 0
+    },
+    user: {
+      type: Object,
+      default: () => {
+        return {}
+      }
+    }
+  },
+  data () {
+    return {
+      truncated: false,
+      truncationLength: 300,
+      review: {
+        author: 'loading',
+        avatar: 'loading',
+        body: 'loading',
+        header: 'loading',
+        likers: [],
+        likes: 0
+      }
+    }
+  },
+  watch: {
+    review (state) {
+      if (state.body.length > 300) {
+        this.truncated = true
+      }
+    }
+  },
+  async mounted () {
+    this.review = this.reviewProp
+    this.review.username = await this.getUsername()
+    this.review.avatar = await this.getAvatar()
+  },
+  methods: {
+    getAvatar () {
+      const imagePath = 'users/' + this.review.author + '/avatar.jpg'
+      return this.$fire.storage.ref().child(imagePath)
+        .getDownloadURL()
+        .then(url => url)
+    },
+    getUsername () {
+      return this.$fire.firestore.collection('users').doc(this.review.author)
+        .get()
+        .then((user) => {
+          const userData = user.data()
+          return userData.username
+        })
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.review {
+  padding: 16px;
+  padding-top: 32px;
+  max-width: 400px;
+  border-radius: 10px;
+  &.alternate {
+    background: $lighter-purple;
+  }
+}
+.flex-container {
+  display: flex;
+}
+.review-avatar {
+  margin-right: 16px;
+}
+.review-author {
+  color: $dimmish-purple;
+  &:hover {
+    color: $saturated-purple;
+  }
+}
+</style>
---
+++ components/Ring.vue
@@ -0,0 +1,44 @@
+
+<template>
+  <div class="ring-container">
+    <!-- https://css-tricks.com/building-progress-ring-quickly/ thanks guys :) -->
+    <svg class="ring" :height="width" :width="width">
+      <circle
+        v-if="shadow"
+        class="ring-circle shadow"
+        :stroke-width="strokeWidth"
+        :stroke="colour"
+        fill="transparent"
+        :r="radius"
+        :cx="position"
+        :cy="position"
+      />
+      <circle
+        class="ring-circle"
+        :style="{ 'stroke-dashoffset': circumference - percent / 100 * circumference, 'stroke-dasharray': `${circumference} ${circumference}` }"
+        :stroke-width="strokeWidth"
+        :stroke="colour"
+        fill="transparent"
+        :r="radius"
+        :cx="position"
+        :cy="position"
+      />
+    </svg>
+    <div class="inside-ring" :style="{ 'width': `${width}px`, 'height': `${width}px`, 'top': `calc(0% + 96px)` }">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Ring',
+  props: {
+    width: {
+      type: Number,
+      default: 128
+    },
+    strokeWidth: {
+      type: Number,
+      default: 4
+    },
