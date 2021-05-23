f4aad9c4041030fd5297edc532c75aa8dc3bba6b
zoujoe <120407910+zoujoe@users.noreply.github.com>
2021-05-23 22:34:49 +0000

    0.23.0

--- components/SearchRelease.vue
+++ components/SearchRelease.vue
@@ -1,3 +1,4 @@
 <template>
   <div class="release">
-  
+    <img v-if="release.image !== 'no-artwork'" class="release-image" :src="release.image" @click="redirectToRelease(release.id)">
+    <div v-else class="release-image no-artwor
