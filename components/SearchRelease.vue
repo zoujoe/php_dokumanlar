5109552e5276634a0fbb9fb22bd8743630dc2bd1
zoujoe <120407910+zoujoe@users.noreply.github.com>
2021-06-01 10:54:06 +0000

    First alpha release

--- components/SearchRelease.vue
+++ components/SearchRelease.vue
@@ -1,4 +1,7 @@
 <template>
   <div class="release">
     <img v-if="release.image !== 'no-artwork'" class="release-image" :src="release.image" @click="redirectToRelease(release.id)">
-    <div v-else class="release-image no-artwor
+    <div v-else class="release-image no-artwork" @click="redirectToRelease(release.id)">
+      <span class="release-title-artworkless">{{ release.title }}</span>
+    </div>
+    <paragraph>{{ release['artist-credit'][0].artist.name }} - {{ relea
