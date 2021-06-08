5e4a6eea789dbdf1f54324ccfb78c611d8f98de6
zoujoe <120407910+zoujoe@users.noreply.github.com>
2021-06-08 14:28:05 +0000

    fixed formatting error

--- components/SearchRelease.vue
+++ components/SearchRelease.vue
@@ -4,4 +4,16 @@
     <div v-else class="release-image no-artwork" @click="redirectToRelease(release.id)">
       <span class="release-title-artworkless">{{ release.title }}</span>
     </div>
-    <paragraph>{{ release['artist-credit'][0].artist.name }} - {{ relea
+    <paragraph>{{ release['artist-credit'][0].artist.name }} - {{ release.title }}</paragraph>
+  </div>
+</template>
+
+<script>
+import paragraph from '@/components/Paragraph.vue'
+
+export default {
+  name: 'SearchResult',
+  components: {
+    paragraph
+  },
+  props: 
