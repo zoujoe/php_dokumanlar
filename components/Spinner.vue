e5196ab2a37e8f5f04870c24d5c2ffef09dfd14e
zoujoe <120407910+zoujoe@users.noreply.github.com>
2021-06-30 14:41:49 +0000

    fixing failing test

--- components/Spinner.vue
+++ components/Spinner.vue
@@ -21,4 +21,6 @@ export default {
 }
 @keyframes spin {
   0% { transform: rotate(0deg); }
-  100% { transform: rotate(360deg
+  100% { transform: rotate(360deg); }
+}
+</style>
---
+++ components/Subheading.vue
@@ -0,0 +1,3 @@
+<template>
+  <div class="subheading" :class="{ noMargin, topMargin }">
+    <h1 :class="[colourMode, { centred, smaller, smallest, titleCase }]" class="subheading-con
