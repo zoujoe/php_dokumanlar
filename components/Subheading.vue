29b8331e5cffbd14efaccf7606262528c6bfce62
zoujoe <120407910+zoujoe@users.noreply.github.com>
2021-07-10 15:38:40 +0000

    Update unit tests

--- components/Subheading.vue
+++ components/Subheading.vue
@@ -1,3 +1,11 @@
 <template>
   <div class="subheading" :class="{ noMargin, topMargin }">
-    <h1 :class="[colourMode, { centred, smaller, smallest, titleCase }]" class="subheading-con
+    <h1 :class="[colourMode, { centred, smaller, smallest, titleCase }]" class="subheading-content" :contenteditable="editable" @input="emitValue" @keydown.enter.prevent>
+      <slot>Subheading content</slot>
+    </h1>
+  </div>
+</template>
+
+<script>
+export default {
+  n
