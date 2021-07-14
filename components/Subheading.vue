9be72d4cfc2fe1c6ed440f15acd9e1102a657a6d
zoujoe <120407910+zoujoe@users.noreply.github.com>
2021-07-14 11:30:29 +0000

    Fixed pep8 issues

--- components/Subheading.vue
+++ components/Subheading.vue
@@ -27,3 +27,13 @@ export default {
     emitValue (element) {
       this.$emit('change', element.target.textContent)
     }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.subheading {
+  margin: 0px;
+  margin-bottom: 8px;
+  &.noMargin
