806c97059a75867bb1d7fd3fa7a309cb75b52646
zoujoe <120407910+zoujoe@users.noreply.github.com>
2021-08-03 06:55:28 +0000

    better format

--- components/VerticalTable.vue
+++ components/VerticalTable.vue
@@ -1,3 +1,8 @@
 <template>
   <div class="vertical-table">
-    <div v-if="!noHeader" class="header
+    <div v-if="!noHeader" class="header">
+      <subheading smaller>
+        <slot name="header" />
+      </subheading>
+    </div>
+    <div class="table-content" :class="{ 'table-content-curved': noHeader, lessLeftPaddi
