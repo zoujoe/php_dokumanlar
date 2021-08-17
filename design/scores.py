2f28c305473976db5f0eb5909c684c4b47774037
zoujoe <120407910+zoujoe@users.noreply.github.com>
2021-08-17 13:52:47 +0000

    Bump version to 0.15.0

--- design/scores.py
+++ design/scores.py
@@ -5,4 +5,9 @@ album2 = [12, 15, 75, 35]
 album3 = [60, 40, 80, 76, 32, 67, 5, 90, 85]
 
 album1median = statistics.median(album1)
-album2median = st
+album2median = statistics.median(album2)
+album3median = statistics.median(album3)
+
+print(album1median)
+print(album2median)
+print(album3median)
