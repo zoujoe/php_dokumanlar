7ba7f77f2e0ea0bb57f3dc3e858c3ef381f7c390
zoujoe <120407910+zoujoe@users.noreply.github.com>
2021-09-25 05:26:26 +0000

    format cleanup

--- functions/index.js
+++ functions/index.js
@@ -323,3 +323,75 @@ exports.getReleaseData = functions.https.onRequest((req, res) => {
           'https://musicbrainz.org/ws/2/release-group/' + id + '?inc=genres&fmt=json',
           headers
         )
+        .then(releaseInfo => releaseInfo.data)
+        .catch((err) => {
+          if (err.response.status === 401) {
+            // some releases do not have genres, and this is often the cause of errors
+            return 401
+          } else {
+            res.status(500).send(err)
+          }
+        })
+    }
+
+    const formatReleaseDate = (releaseDate) => {
+      // sample data: 1996-05-28 (28th of May, 1996 [Merzbow - Pulse Demon])
+      const year = releaseDate.substring(0, 4)
+      const month = releaseDate.substring(5, 7)
+      const day = releaseDate.substring(8)
+
+      const formattedDate = moment(releaseDate).format('DD MMMM')
+
+      return [day, month, year, formattedDate]
+    }
+
+    const getReleaseFromDatabase = () => {
+      return admin.firestore().collection('releases').doc(id)
+        .get()
+        .then((res) => {
+          return res.data()
+        })
+        .catch(() => {
+          return false
+        })
+    }
+
+    const getReleaseFromMusicbrainz = () => {
+      axios
+        .get(
+          'https://musicbrainz.org/ws/2/release-group/?query=mbid:"' + id + '"&fmt=json',
+          headers
+        )
+        .then(async (queryResult) => {
+          const release = queryResult.data['release-groups'][0]
+          const releaseInfo = await getReleaseInfo()
+          const artwork = await getReleaseArtwork(release)
+          release.image = artwork[0]
+          release.imageHQ = artwork[1]
+          release.genres = releaseInfo.genres
+          const firstReleaseDate = releaseInfo['first-release-date']
+          const formattedReleaseDate = formatReleaseDate(firstReleaseDate)
+          release.firstReleaseDate = formattedReleaseDate
+          release.lists = []
+          res.status(200).send(release)
+        })
+        .catch((err) => {
+          res.status(500).send(err)
+        })
+    }
+
+    const getRelease = async () => {
+      const release = await getReleaseFromDatabase()
+      if (release) {
+        res.status(200).send(release)
+      } else {
+        getReleaseFromMusicbrainz()
+      }
+    }
+
+    getRelease()
+  })
+})
+admin.initializeApp({
+  credential: admin.credential.cert(require('./admin.json'))
+})
