ae656c5342afa9de97f2fb49882eb081e2dfda6e
zoujoe <120407910+zoujoe@users.noreply.github.com>
2021-11-02 06:50:41 +0000

    title changes

--- nuxt.config.js
+++ nuxt.config.js
@@ -51,3 +51,100 @@ module.exports = {
   /*
    ** Nuxt.js dev-modules
    */
+  buildModules: [
+    // Doc: https://github.com/nuxt-community/eslint-module
+    '@nuxtjs/eslint-module',
+    '@nuxtjs/dotenv'
+  ],
+  /*
+   ** Nuxt.js modules
+   */
+  modules: [
+    '@nuxtjs/pwa',
+    '@nuxtjs/axios',
+    '@nuxtjs/proxy',
+    '@nuxtjs/style-resources',
+    [
+      '@nuxtjs/firebase',
+      {
+        config: {
+          apiKey: process.env.FIREBASE_API_KEY,
+          authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+          databaseURL: process.env.FIREBASE_DATABASE_URL,
+          projectId: process.env.FIREBASE_PROJECT_ID,
+          storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+          appId: process.env.APP_ID
+        },
+        services: {
+          auth: true,
+          firestore: true,
+          functions: true,
+          storage: true
+        }
+      }
+    ]
+  ],
+  axios: {
+    proxy: true
+  },
+  proxy: {
+    '/wikidataAPI': {
+      target: 'https://wikidata.org/w/api.php',
+      pathRewrite: {
+        '^/wikidataAPI': ''
+      },
+      changeOrigin: true
+    },
+    '/wikimediaCommons': {
+      target: 'https://commons.wikimedia.org/w/thumb.php',
+      pathRewrite: {
+        '^/wikimediaCommons': ''
+      }
+    },
+    '/musicBrainzAPI': {
+      target: 'https://musicbrainz.org/ws/2',
+      pathRewrite: {
+        '^/musicBrainzAPI': ''
+      }
+    },
+    '/archive': {
+      target: 'https://archive.org/',
+      pathRewrite: {
+        '^/archive': ''
+      },
+      followRedirects: true
+    },
+    '/coverArtArchive': {
+      target: 'https://coverartarchive.org/',
+      pathRewrite: {
+        '^/coverArtArchive': ''
+      },
+      followRedirects: true
+    }
+  },
+  styleResources: {
+    scss: ['~/assets/scss/palette.scss']
+  },
+  /*
+   ** Build configuration
+   */
+  build: {
+    /*
+     ** You can extend webpack config here
+     */
+    publicPath: '/public/',
+    extend (config, ctx) {
+      if (ctx.dev && ctx.isClient) {
+        config.module.rules.push({
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules)/,
+          options: {
+            fix: true
+          }
+        })
+      }
+    }
+  }
+}
