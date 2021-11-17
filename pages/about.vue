c68f114de9071b48b8f9dd55b19b980568d026ea
zoujoe <120407910+zoujoe@users.noreply.github.com>
2021-11-17 15:21:05 +0000

    Fixing code violations

---
+++ pages/about.vue
@@ -0,0 +1,86 @@
+
+<template>
+  <main class="about">
+    <article-content>
+      <masthead centred smaller full-width>
+        About tasteful
+      </masthead>
+      <paragraph-container>
+        <subheading>What is tasteful?</subheading>
+        <paragraph>
+          tasteful is a platform that allows you to rate, review, and catalog
+          your music collection. Although the platform is comparable to the
+          (fantastic!) websites
+          <a href="https://rateyourmusic.com">Rate Your Music</a> and
+          <a href="https://albumoftheyear.org">Album of the Year</a>, it
+          differentiates itself through its comprehensive array of additional
+          features, including blogging.
+        </paragraph>
+      </paragraph-container>
+      <paragraph-container>
+        <subheading>Why should I choose tasteful?</subheading>
+        <paragraph>
+          You don't have to; there's nothing stopping you from using multiple
+          services! tasteful is, at this stage, a passion project.
+        </paragraph>
+        <paragraph>
+          Having said that, here's some reasons as to why you
+          <em>should</em> consider trying us out:
+        </paragraph>
+        <list>
+          <li>🦉 Dark mode.</li>
+          <li>📝 Blog-like profiles.</li>
+          <li>👨‍👩‍👧 A tight-knit, small community.</li>
+          <li>🎨 An intuitive, vibrant interface.</li>
+          <li>🕵️‍♀️ An easy-to-use but powerful search engine.</li>
+          <li>
+            📊 Built-in chart making, akin to 'topsters'.
+            <list>
+              <li>
+                🤖 This process can be automated, and may use data from your
+                ratings.
+              </li>
+            </list>
+          </li>
+          <li>
+            🎼 Comprehensive databases.
+            <list>
+              <li>
+                👩‍🏫 tasteful's music database stems from pre-existing sources,
+                unlike similar solutions.
+              </li>
+            </list>
+          </li>
+        </list>
+      </paragraph-container>
+      <paragraph-container>
+        <subheading>
+          How can I import my ratings/reviews from AOTY or RYM?
+        </subheading>
+        <paragraph>
+          At one point, the answer to this was a resounding no, but following some research, importing both ratings and reviews from RYM will be possible without violating their terms of service. tasteful can't guarantee migration from AOTY, but we're still looking into it.
+        </paragraph>
+      </paragraph-container>
+      <paragraph-container>
+        <subheading>When does it open?</subheading>
+        <paragraph>
+          I have <strong>no idea.</strong> tasteful is a very real product,
+          however, and is open source - you can view the code
+          <a href="https://github.com/lachlantula/tasteful">here</a>
+          and track my progress. The vast majority of the stuff that is not yet
+          available on the public website is still in development locally, and
+          will be committed to the repository once tasteful is closer to launch.
+          Contributions are greatly appreciated, however, and you will be
+          rewarded accordingly with a 'Contributor' badge.
+        </paragraph>
+      </paragraph-container>
+      <paragraph-container>
+        <subheading>How much does it cost?</subheading>
+        <paragraph>
+          tasteful will be largely free. Users may pay more for some additional
+          features, but they are mostly customisation-based. We will be
+          publishing more information regarding pricing in the near future, but
+          there will be three tiers, ranging from a $1 to $3 subscription,
+          renewed on a monthly basis. Lower-tier benefits may include an ad-free
+          interface, while higher tiers may get additional promotion for their
+          reviews.
