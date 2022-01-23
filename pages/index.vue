4c16ef348d33855488bfcdb4a91c1e0a90809129
zoujoe <120407910+zoujoe@users.noreply.github.com>
2022-01-23 22:34:09 +0000

    indented

--- pages/index.vue
+++ pages/index.vue
@@ -1,1 +1,7 @@
-<t
+<template>
+  <main>
+    <div class="index">
+      <article-content>
+        <paragraph-container>
+          <paragraph>
+            kind of working. didn't intend for that to be the case (as
