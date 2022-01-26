c374c1bbc76c81d34b216bb8be6513c1fb1d8e26
zoujoe <120407910+zoujoe@users.noreply.github.com>
2022-01-26 10:51:30 +0000

    Tests refactored

--- pages/index.vue
+++ pages/index.vue
@@ -4,4 +4,7 @@
       <article-content>
         <paragraph-container>
           <paragraph>
-            kind of working. didn't intend for that to be the case (as
+            kind of working. didn't intend for that to be the case (as you can probably tell) but here we are!
+          </paragraph>
+          <nuxt-link to="/release/aa366795-92d7-4d67-91fa-6741f82a4858">
+            this page should (mostly
