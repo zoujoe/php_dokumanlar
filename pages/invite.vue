1b28e118ca41e76d24579fbc575096c0dd28b2cf
zoujoe <120407910+zoujoe@users.noreply.github.com>
2022-02-20 05:28:45 +0000

    More updates to README.md

--- pages/invite.vue
+++ pages/invite.vue
@@ -54,3 +54,135 @@
             <paragraph>
               An <strong>invite seed</strong> is an eight-character set of alphanumeric characters that is used to significantly reduce the
               likelihood of a randomly-guessed invite code from being successful in developing an Invitation.
+              If you're familiar with cryptography, this is a salt.
+            </paragraph>
+            <paragraph>
+              An <strong>invite code</strong> is a code of randomly-generated words that is one of the two parts (the other being the invite seed)
+              required in developing an Invitation.
+            </paragraph>
+            <paragraph>
+              An <strong>Invitation</strong> is 'go-ahead' given by the tasteful code that offers the opportunity to sign up for the website.
+              This go-ahead is generated following verification that when your invite seed and code are hashed in unison that they reach the same result
+              as what is in our database.
+            </paragraph>
+          </paragraph-container>
+          <paragraph-container>
+            <subheading>How do I get an invite seed and code?</subheading>
+            <paragraph>
+              Invite codes are given out rarely to select members of the tasteful community.
+              There is no way to get one other than to know somebody who has one and is generous enough to give it to you.
+              It may only be used once.
+            </paragraph>
+          </paragraph-container>
+          <paragraph-container>
+            <subheading>Why is this all so complicated for seemingly no reason?</subheading>
+            <paragraph>So losers like you don't get in.</paragraph>
+          </paragraph-container>
+        </article-content>
+      </div>
+    </div>
+    <div v-else class="invite">
+      <article-content>
+        <paragraph-container>
+          <subheading>You already have an account, silly!</subheading>
+          <paragraph>
+            You may have meant to go to your
+            <nuxt-link to="/user/settings">
+              user settings</nuxt-link> <!-- eslint-disable-line -->
+            to generate or share your Invitation?
+          </paragraph>
+        </paragraph-container>
+      </article-content>
+    </div>
+  </main>
+</template>
+
+<script>
+import crypto from 'crypto'
+import FingerprintIcon from 'vue-material-design-icons/Fingerprint.vue'
+import KeyIcon from 'vue-material-design-icons/Key.vue'
+import ArticleContent from '~/components/ArticleContent.vue'
+import ParagraphContainer from '~/components/ParagraphContainer.vue'
+import Paragraph from '~/components/Paragraph.vue'
+import Masthead from '~/components/Masthead.vue'
+import TextInput from '~/components/TextInput.vue'
+import Subheading from '~/components/Subheading.vue'
+import DividedContainer from '~/components/DividedContainer.vue'
+import SubmitButton from '~/components/SubmitButton.vue'
+import Blur from '~/components/Blur.vue'
+
+export default {
+  name: 'Invite',
+  components: {
+    FingerprintIcon,
+    KeyIcon,
+    TextInput,
+    ArticleContent,
+    ParagraphContainer,
+    Paragraph,
+    Masthead,
+    Subheading,
+    DividedContainer,
+    SubmitButton,
+    Blur
+  },
+  data () {
+    return {
+      inviteSeed: '',
+      inviteCode: '',
+      loading: true,
+      flashRed: false,
+      inviteSubmitted: false
+    }
+  },
+  computed: {
+    colourMode () {
+      return this.$store.state.theme.colourMode
+    },
+    user () {
+      return this.$store.state.login.user
+    }
+  },
+  mounted () {
+    setTimeout(() => {
+      this.loading = false
+    }, 2000)
+  },
+  methods: {
+    testInvitation () {
+      const invitation = crypto.createHash('sha256').update(this.inviteCode + this.inviteSeed).digest('hex')
+      this.$fire.firestore.collection('invites').doc(invitation).get()
+        .then((res) => {
+          if (res.exists && (res.data().used === false)) {
+            this.submitInvitation(invitation)
+          } else {
+            this.flashRed = true
+          }
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+    },
+    submitInvitation (invitation) {
+      this.$fire.firestore.collection('invites').doc(invitation).set({
+        used: true
+      })
+        .then(() => {
+          // User may now sign up
+          this.$store.commit('login/setSignup', true)
+          this.inviteSubmitted = true
+        })
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.invite {
+  padding-left: 3vw;
+  padding-right: 3vw;
+  display: flex;
+  justify-content: center;
+  padding-bottom: 3vh;
+}
+</style>
---
+++ pages/preregistration.vue
@@ -0,0 +1,17 @@
+
+<template>
+  <div :class="colourMode" class="index">
+    <resize-observer @notify="handleResize" />
+    <main>
+      <masthead :full-width="isFullWidth">
+        {{ mastheadText }}
+      </masthead>
+      <section class="call-to-action-box">
+        <h2>Preregister an account now for early access in the near future.</h2>
+        <form
+          id="preregistration-input-box"
+          name="preregistration"
+          @submit.prevent="onSignup"
+        >
+          <div class="call-to-action-content">
+            <div class="email-input">
