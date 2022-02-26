5335d1a35be6fa7c7383a79d7c129586fe1a8bc2
zoujoe <120407910+zoujoe@users.noreply.github.com>
2022-02-26 08:29:01 +0000

    Adding source

--- pages/release/_release/index.vue
+++ pages/release/_release/index.vue
@@ -1,4 +1,5 @@
 <template>
   <main class="release">
     <transition name="fade">
-      <modal v-if="showAdd
+      <modal v-if="showAddToListModal" larger-left-side scrollable :image-for-right-side="release.imageHQ" @closeModal="showAddToListModal = false; showCreateListModal = false; showAdjustPositionInListModal = false">
+      
