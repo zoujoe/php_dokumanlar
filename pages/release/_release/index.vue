266eaffedf0ccaea331077250fb906d109604459
zoujoe <120407910+zoujoe@users.noreply.github.com>
2022-02-26 17:02:18 +0000

    Regenerate gemspec for version 1.0.4

--- pages/release/_release/index.vue
+++ pages/release/_release/index.vue
@@ -2,4 +2,7 @@
   <main class="release">
     <transition name="fade">
       <modal v-if="showAddToListModal" larger-left-side scrollable :image-for-right-side="release.imageHQ" @closeModal="showAddToListModal = false; showCreateListModal = false; showAdjustPositionInListModal = false">
-      
+        <template #heading>
+          <masthead smaller>
+            <span v-if="showCreateListModal">Create new list</span>
+            <span v-else-if="showAdjustPositio
