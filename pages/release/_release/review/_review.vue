f65684cb23ff1037d15e74d71b8631ce70ec5eaf
zoujoe <120407910+zoujoe@users.noreply.github.com>
2022-11-08 05:39:00 +0000

    Spring cleaning.

--- pages/release/_release/review/_review.vue
+++ pages/release/_release/review/_review.vue
@@ -2,4 +2,10 @@
   <main class="release-review">
     <transition name="fade">
       <modal v-if="showAddToListModal" scrollable :image-for-right-side="release.imageHQ" @closeModal="showAddToListModal = false">
- 
+        <template #heading>
+          <masthead smaller>
+            Add release to list
+          </masthead>
+        </template>
+        <template #left>
+          <dropdo
