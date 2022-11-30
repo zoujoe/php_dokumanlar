787135984bbf80bd7cc58c1a94e1553dc732f629
zoujoe <120407910+zoujoe@users.noreply.github.com>
2022-11-30 19:30:31 +0000

    Updated generated docs

--- pages/release/_release/review/_review.vue
+++ pages/release/_release/review/_review.vue
@@ -191,4 +191,9 @@ export default {
         .getDownloadURL()
         .then(url => url)
     },
-    
+    getUsername (id) {
+      return this.$fire.firestore.collection('users').doc(id)
+        .get()
+        .then((user) => {
+          const userData = user.data()
+          return user
