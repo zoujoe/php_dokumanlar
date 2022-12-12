1ff558251c2b5fc329ed2a29cc5757b52069e958
zoujoe <120407910+zoujoe@users.noreply.github.com>
2022-12-12 10:55:09 +0000

    Blank line

--- pages/success.vue
+++ pages/success.vue
@@ -11,4 +11,10 @@
           grateful from the bottom of my heart. Please be aware, though, that
           any nicknames proposed in pre-registration may be disregarded at my
           own discretion. This will be largely limited to merely hate speech and
-          impersonation of an official tasteful acco
+          impersonation of an official tasteful account.
+        </paragraph>
+      </paragraph-container>
+      <paragraph-container>
+        <subheading>What's next?</subheading>
+        <paragraph>
+          tasteful has  a long road ahead, but b
