979580724be6ccfa27500196b371fe4c54a019a4
zoujoe <120407910+zoujoe@users.noreply.github.com>
2022-12-17 21:05:56 +0000

    Fixes bugs

--- server/index.js
+++ server/index.js
@@ -23,3 +23,13 @@ async function start () {
   }
 
   // Give nuxt middleware to express
+  app.use(nuxt.render)
+
+  // Listen the server
+  app.listen(port, host)
+  consola.ready({
+    message: `Server listening on http://${host}:${port}`,
+    badge: true
+  })
+}
+start()
